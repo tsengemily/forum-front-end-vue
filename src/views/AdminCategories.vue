8e5ff0f2de617ba5fab50f021e2f872d20d1a485
Emily <tsengemily818@gmail.com>
2021-02-25 15:44:43 +0800

    Add API - admin.categories.update and integrate in AdminCategories

--- src/views/AdminCategories.vue
+++ src/views/AdminCategories.vue
@@ -203,10 +203,22 @@ export default {
         return category
       })
     },
-     updateCategory ({ categoryId, name }) {
-      // TODO: 透過 API 去向伺服器更新餐廳類別名稱
-      this.toggleIsEditing(categoryId)
-      console.log(name)
+    async updateCategory ({ categoryId, name }) {
+      try {
+        const { data } = await adminAPI.categories.update({ categoryId, name })
+        
+        if (data.status !== 'success') {
+          throw new Error(data.message)
+        }
+
+        this.toggleIsEditing(categoryId)
+      } catch (error) {
+        console.log('error', error)
+        Toast.fire({
+          icon: 'error',
+          title: '更新失敗，請稍後再試'
+        })
+      }  
     },
     handleCancel (categoryId) {
       this.categories = this.categories.map(category => {
