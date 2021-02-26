f2312e634670626af9622ba7cde37ac391affa45
Emily <tsengemily818@gmail.com>
2021-02-26 15:15:45 +0800

    fix: dispatch fetchCurrentUser action beforeEach router

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import usersAPI from '../apis/users'
 
 Vue.use(Vuex)
 
@@ -24,6 +25,26 @@ export default new Vuex.Store({
       state.isAuthenticated = true
     },
   },
-  actions: {},
+  actions: {
+    //在action中可以透過參數的方式取得commit的方法
+    async fetchCurrentUser({ commit }) {
+      try {
+        const { data } = await usersAPI.getCurrentUser()
+
+        const { id, name, email, image, isAdmin } = data
+
+        commit('setCurrentUser', {
+          id,
+          name,
+          email,
+          image,
+          isAdmin,
+        })
+      } catch (error) {
+        console.log('error', error)
+        console.error('can not fetch user information')
+      }
+    },
+  },
   modules: {},
 })
